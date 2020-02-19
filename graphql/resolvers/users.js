5595bd810b767babaa90ec376978206acb163120
Michael <michaeltroya13@gmail.com>
2020-02-19 11:49:19 -0500

    add registration validation

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,14 +1,39 @@
 require('dotenv').config();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { UserInputError } = require('apollo-server');
 
 const User = require('../../models/User');
+
+const { validateRegisterData } = require('../../util/validators');
 
 module.exports = {
   Mutation: {
     async register(_, { registerInput: { username, email, password, confirmPassword } }, context, info) {
-      password = await bcrypt.hash(password, 12);
+      const { errors, isValid } = validateRegisterData(username, email, password, confirmPassword);
 
+      if (!isValid) {
+        throw new UserInputError('Errors', { errors });
+      }
+
+      const checkUsername = await User.findOne({ username });
+      const checkEmail = await User.findOne({ email });
+
+      if (checkUsername) {
+        throw new UserInputError('Username is taken', {
+          errors: {
+            username: 'Username is taken'
+          }
+        });
+      } else if (checkEmail) {
+        throw new UserInputError('Email is taken', {
+          errors: {
+            email: 'Email is taken'
+          }
+        });
+      }
+
+      password = await bcrypt.hash(password, 12);
       const newUser = new User({
         email,
         username,
@@ -17,8 +42,6 @@ module.exports = {
       });
 
       const res = await newUser.save();
-
-      console.log(res);
 
       const token = jwt.sign(
         {
---
+++ util/validators.js
@@ -0,0 +1,37 @@
+const isEmpty = require('is-empty');
+const validator = require('validator');
+
+exports.validateRegisterData = (username, email, password, confirmPassword) => {
+  const errors = {};
+  const regex = /^[A-Za-z0-9 ]+$/;
+  const usernameTest = regex.test(username);
+
+  if (isEmpty(email)) {
+    errors.email = 'Email can not be empty';
+  } else if (!validator.isEmail(email)) {
+    errors.email = 'Must be a valid email';
+  }
+
+  if (isEmpty(password)) {
+    errors.password = 'Password can not be empty';
+  } else if (password.length < 6) {
+    errors.password = 'Password must be a minimum of 6 characters';
+  }
+
+  if (password !== confirmPassword) {
+    errors.password = 'Passwords must match';
+  }
+
+  if (isEmpty(username)) {
+    errors.username = 'Username can not be empty';
+  } else if (username.length < 4) {
+    errors.username = 'Username must be a minimum of 4 characters';
+  } else if (!usernameTest) {
+    errors.username = 'Username can only have letters and numbers';
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
